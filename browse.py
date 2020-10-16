5478a1a852faf9216a6a94649c1ed6a33624176a
Mat <mnantel@gmail.com>
2020-10-16 11:35:21 -0400

    Update browse.py

--- browse.py
+++ browse.py
@@ -3,10 +3,14 @@ import time
 
 urls = [
     'https://www.facebook.com',
-    'https://www.google.com'
+    'https://www.google.com',
+    'https://www.fortinet.com',
+    'https://radiocanada.ca',
+    'https://lotoquebec.ca'
 ]
 
 while True:
     for u in urls:
+        print("Getting: "+u)
         r = requests.get(u, allow_redirects=True)
     time.sleep(2)
